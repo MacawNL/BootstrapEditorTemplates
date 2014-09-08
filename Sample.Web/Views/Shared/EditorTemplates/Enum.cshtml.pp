cae2eed9936710cace893b16b608a1d21a59f055
Maarten Sikkema <Maarten.Sikkema@macaw.nl>
2014-09-08 14:26:06 +0200

    Update NuGet build params, cleanup .pp files

--- Sample.Web/Views/Shared/EditorTemplates/Enum.cshtml.pp
+++
@@ -1,9 +0,0 @@
-﻿@using $rootnamespace$.Utility
-@model object
-
-
-<div class="form-group@(Html.ValidationErrorFor(m => m, " has-error"))">
-    @Html.LabelFor(m => m, new { @class = "control-label" })
-    @Html.EnumDropDownListFor(m => m, new { @class = "form-control" })
-    @Html.ValidationMessageFor(m => m, null, new { @class = "help-block" })
-</div>
